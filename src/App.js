7e685230860191d92f32b5be8f288d9b2054bb59
elyor doniyorov <doniyorovelyor@gmail.com>
2021-11-15 02:12:12 +0500

    Style App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import store from '../redux/configureStore';
+import store from './redux/configureStore';
 import Greeting from './Greeting';
 import './App.css';
 
