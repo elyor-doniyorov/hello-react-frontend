b5731e609c1bacbc34e22ec472f44efd677f038c
ELYOR <doniyorovelyor@gmail.com>
2021-11-23 20:18:58 +0500

    Revert "Hello-React-Frontend"

--- src/redux/salutes/Api.js
+++
@@ -1,8 +0,0 @@
-const apiSalutes = async () => {
-  const request = await fetch('http://localhost:3000/api/salutes');
-  const response = await request.json();
-  const salute = await response.body;
-  return salute;
-};
-
-export default apiSalutes;
